91708f14e84aa465511590fb9af929446916740c
MinhVanK22 <minhvan642002@gmail.com>
2023-05-23 14:47:50 +0700

    Modify code

--- Objects/init.d
+++
@@ -1,9 +0,0 @@
-.\objects\init.o: Init.c
-.\objects\init.o: D:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.4.0\Device\Include\stm32f10x.h
-.\objects\init.o: .\RTE\_Target_1\RTE_Components.h
-.\objects\init.o: D:\Keil_v5\ARM\PACK\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h
-.\objects\init.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\init.o: D:\Keil_v5\ARM\PACK\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\init.o: D:\Keil_v5\ARM\PACK\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\init.o: D:\Keil_v5\ARM\PACK\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
-.\objects\init.o: D:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.4.0\Device\Include\system_stm32f10x.h
